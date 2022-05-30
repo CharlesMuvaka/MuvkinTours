fe7293b98680a0181ff9222adbe827a3c8cc6b1e
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-05-30 15:59:05 +0300

    create a test to validate the text in the order button

--- app/src/test/java/com/example/muvkintours/MainActivityTest.java
+++ app/src/test/java/com/example/muvkintours/MainActivityTest.java
@@ -1,2 +1,41 @@
-package com.example.muvkintours;public class MainActivityTest {
+package com.example.muvkintours;
+
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+    MainActivity activity;
+
+    @Before
+    public void setUp() throws Exception{
+        activity = Robolectric.buildActivity(MainActivity.class).create().start().resume().get();
+
+    }
+
+    @Test
+    public void checkAppLogo_returnsAppLogo(){
+
+        TextView logo = activity.findViewById(R.id.logo);
+        String text = "MuvKinTours";
+
+        assertEquals(text,logo.getText());
+    }
+
+    @Test
+    public void getOrderButtonText_ReturnsString(){
+
+        Button order = activity.findViewById(R.id.order);
+        String text = "Order";
+
+        assertEquals(text, order.getText());
+    }
 }
