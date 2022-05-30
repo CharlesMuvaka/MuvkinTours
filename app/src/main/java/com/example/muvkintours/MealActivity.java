bd140eed0e1acac69eaf8c0035546d20ed71ca75
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-05-30 15:57:13 +0300

    create meals and images array that will be displayed in the grid view

--- app/src/main/java/com/example/muvkintours/MealActivity.java
+++ app/src/main/java/com/example/muvkintours/MealActivity.java
@@ -2,13 +2,81 @@ package com.example.muvkintours;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import com.example.muvkintours.Adapters.GridAdapter;
+import com.example.muvkintours.databinding.ActivityMealBinding;
 
 public class MealActivity extends AppCompatActivity {
+    ActivityMealBinding mealBind;
+     String[] meals = new String[]{
+             "Pilau",
+             "Rice",
+             "Githeri",
+             "beer",
+             "juice",
+             "Guavas",
+             "Ugali",
+             "Burger",
+             "Samosa",
+             "Samosa",
+             "Samosa",
+             "Samosa",
+             "Samosa",
+             "Samosa",
+             "Rice",
+             "Rice",
+             "Rice",
+             "Rice",
+             "Rice"
+     };
+
+     int[] images = new int[]{
+             R.drawable.pilau,
+             R.drawable.rice,
+             R.drawable.githeri,
+             R.drawable.beer,
+             R.drawable.githeri,
+             R.drawable.fruits,
+             R.drawable.ugali,
+             R.drawable.rice,
+             R.drawable.samosa,
+             R.drawable.samosa,
+             R.drawable.samosa,
+             R.drawable.samosa,
+             R.drawable.samosa,
+             R.drawable.samosa,
+             R.drawable.rice,
+             R.drawable.rice,
+             R.drawable.rice,
+             R.drawable.rice,
+             R.drawable.rice
+
+     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_meal);
+        mealBind = ActivityMealBinding.inflate(getLayoutInflater());
+        setContentView(mealBind.getRoot());
+
+        Intent newIntent = getIntent();
+        String name = newIntent.getStringExtra("name");
+        mealBind.mealText.setText("Welcome to the list of our available meals " + name);
+
+
+        GridAdapter adp = new GridAdapter(MealActivity.this,meals ,images);
+        mealBind.grid.setAdapter(adp);
+
+        mealBind.grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MealActivity.this, name + "its not done wait!!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
