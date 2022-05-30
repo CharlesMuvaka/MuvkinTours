13fb0a543820b72265b3f4cda11597717db1b2bb
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-05-30 16:05:17 +0300

    add tests to validate the hints present in the sign up activity edittexts

--- app/src/test/java/com/example/muvkintours/SignUpActivityTest.java
+++ app/src/test/java/com/example/muvkintours/SignUpActivityTest.java
@@ -2,11 +2,30 @@ package com.example.muvkintours;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
+import com.google.android.material.textfield.TextInputLayout;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
 public class SignUpActivityTest {
+    SignUpActivity activity;
 
     @Before
     public void setUp() throws Exception {
+
+        activity = Robolectric.buildActivity(SignUpActivity.class).create().start().resume().get();
+
+    }
+
+    @Test
+    public void getHint_Returns_true(){
+
+        TextInputLayout user = activity.findViewById(R.id.userName);
+
+        assertEquals("Enter you full name", user.getEditText().getHint());
     }
 }
