4e3254d28a45abe9376c3bb1818cf0f935aad249
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-05-30 15:58:19 +0300

    create a validator class to validate user inputs in the sign up Activity

--- app/src/main/java/com/example/muvkintours/Validator.java
+++ app/src/main/java/com/example/muvkintours/Validator.java
@@ -1,4 +1,38 @@
 package com.example.muvkintours;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class Validator {
+
+    public boolean validateName(TextInputLayout userName){
+
+        if (userName.getEditText().getText().toString().isEmpty()){
+            userName.setError("Please Enter your username");
+            return false;
+        }else{
+            userName.setError(null);
+            userName.setErrorEnabled(false);
+            return true;
+
+        }
+
+    }
+
+    public boolean validatePhone(TextInputLayout userPhone){
+
+        if (userPhone.getEditText().getText().toString().isEmpty()){
+            userPhone.setError("Please Enter your mobile Number");
+            return false;
+        }else if (userPhone.getEditText().getText().toString().length() != 10){
+            userPhone.setError("Phone number should be Ten digits");
+            return false;
+        }
+        else{
+            userPhone.setError(null);
+            userPhone.setErrorEnabled(false);
+            return true;
+
+        }
+
+    }
 }
