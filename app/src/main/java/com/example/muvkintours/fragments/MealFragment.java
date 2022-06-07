3c4b9ff0b7b323f92a32ffdc192c74e0ab9b3ae7
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-07 15:02:03 +0300

    create a reusable fragment that will display individual data of an individual meal category

--- app/src/main/java/com/example/muvkintours/fragments/MealFragment.java
+++ app/src/main/java/com/example/muvkintours/fragments/MealFragment.java
@@ -7,43 +7,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.example.muvkintours.databinding.FragmentMealBinding;
+import com.example.muvkintours.models.Category;
+import com.squareup.picasso.Picasso;
 
-import com.example.muvkintours.R;
+import org.parceler.Parcels;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MealFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class MealFragment extends Fragment {
+    FragmentMealBinding mealBinding;
+    Category category;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public MealFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MealFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MealFragment newInstance(String param1, String param2) {
+
+    public static MealFragment newInstance(Category category) {
         MealFragment fragment = new MealFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putParcelable("category", Parcels.wrap(category));
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,8 +38,7 @@ public class MealFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            category = Parcels.unwrap(getArguments().getParcelable("category"));
         }
     }
 
@@ -61,6 +46,15 @@ public class MealFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meal, container, false);
+
+        mealBinding = FragmentMealBinding.inflate(inflater, container, false);
+        View v = mealBinding.getRoot();
+
+        Picasso.get().load(category.getStrCategoryThumb()).into(mealBinding.image);
+        mealBinding.name.setText(category.getStrCategory());
+        mealBinding.desc.setText(category.getStrCategoryDescription());
+        mealBinding.ingNum.setText("IngredientId: " +category.getIdCategory());
+
+        return v;
     }
 }
