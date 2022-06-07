edd49b4b15b2d1451961651a7dd161c8bf4cc539
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-07 15:05:35 +0300

    create a singleton that will make http requests to the api

--- app/src/main/java/com/example/muvkintours/mealApi/RetrofitClient.java
+++ app/src/main/java/com/example/muvkintours/mealApi/RetrofitClient.java
@@ -1,2 +1,20 @@
-package com.example.muvkintours.mealApi;public class RetrofitClient {
+package com.example.muvkintours.mealApi;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static Retrofit retrofit = null;
+
+
+    public static MealAPi getClient(){
+
+        if (retrofit == null){
+
+            retrofit = new Retrofit.Builder().baseUrl(Constants.URL).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+
+        return retrofit.create(MealAPi.class);
+    }
 }
