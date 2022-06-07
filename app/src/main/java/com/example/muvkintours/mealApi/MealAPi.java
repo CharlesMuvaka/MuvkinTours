346fad9e25eb522ca1f1ff1371b6a5a683a2e6ec
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-07 15:04:50 +0300

    add a method to get all meal categories from the meal api

--- app/src/main/java/com/example/muvkintours/mealApi/MealAPi.java
+++ app/src/main/java/com/example/muvkintours/mealApi/MealAPi.java
@@ -1,2 +1,13 @@
-package com.example.muvkintours.mealApi;public class MealAPi {
+package com.example.muvkintours.mealApi;
+
+import com.example.muvkintours.models.Meal;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MealAPi {
+
+    @GET("categories.php")
+    Call<Meal> getCategories();
 }
