76f1b610ea829a9d1a4c23f04d02983d87c43af1
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-07 15:00:40 +0300

    sets an adapter to the view pager to bind fragments and their data

--- app/src/main/java/com/example/muvkintours/MealDetailActivity.java
+++ app/src/main/java/com/example/muvkintours/MealDetailActivity.java
@@ -1,14 +1,33 @@
 package com.example.muvkintours;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
 
 import android.os.Bundle;
 
+import com.example.muvkintours.Adapters.MealPagerAdapter;
+import com.example.muvkintours.databinding.ActivityMealDetailBinding;
+import com.example.muvkintours.models.Category;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
 public class MealDetailActivity extends AppCompatActivity {
+    ActivityMealDetailBinding detBind;
+    MealPagerAdapter adp;
+    List<Category> allCats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_meal_detail);
+        detBind = ActivityMealDetailBinding.inflate(getLayoutInflater());
+        setContentView(detBind.getRoot());
+
+        int position = getIntent().getIntExtra("position", 0);
+        allCats = Parcels.unwrap(getIntent().getParcelableExtra("cats"));
+        adp = new MealPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, allCats );
+        detBind.viewPager.setAdapter(adp);
+        detBind.viewPager.setCurrentItem(position);
     }
 }
