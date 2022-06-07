162075a2498686176cec056f549da54255a7df1f
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-07 15:15:56 +0300

    add test to confirm the text inside the first paragraph

--- app/src/test/java/com/example/muvkintours/MealActivityTest.java
+++ app/src/test/java/com/example/muvkintours/MealActivityTest.java
@@ -2,15 +2,32 @@ package com.example.muvkintours;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.runner.RunWith;
+import android.widget.Button;
+import android.widget.TextView;
 
-@RunWith(RobolectricR)
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
 public class MealActivityTest {
     MealActivity act;
 
     @Before
-    public void setUp() throws Exception {
-        act =
+    public void setUp() {
+        act = Robolectric.buildActivity(MealActivity.class).create().start().resume().get();
     }
+
+    @Test
+    public void getParagraphText_ReturnsString(){
+
+        TextView order = act.findViewById(R.id.mealText);
+        String text = "WELCOMEnull, click on the category to read more...";
+
+        assertEquals(text, order.getText());
+    }
+
+
 }
