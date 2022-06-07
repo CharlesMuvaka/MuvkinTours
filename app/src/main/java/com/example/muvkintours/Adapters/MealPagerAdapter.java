8fb6be4fefa1ccc88c6991356f03ae4d20a58790
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-07 14:52:02 +0300

    add methods to bind data to the meal detail fragment

--- app/src/main/java/com/example/muvkintours/Adapters/MealPagerAdapter.java
+++ app/src/main/java/com/example/muvkintours/Adapters/MealPagerAdapter.java
@@ -1,2 +1,43 @@
-package com.example.muvkintours.Adapters;public class MealPagerAdapter {
+package com.example.muvkintours.Adapters;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.muvkintours.fragments.MealFragment;
+import com.example.muvkintours.models.Category;
+
+import java.util.List;
+
+public class MealPagerAdapter extends FragmentPagerAdapter {
+
+    private List<Category> allCats;
+
+    public MealPagerAdapter(@NonNull FragmentManager fm, int behavior, List<Category> cats ) {
+        super(fm, behavior);
+
+        this.allCats = cats;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        MealFragment frag = MealFragment.newInstance(allCats.get(position));
+
+        return frag;
+    }
+
+    @Override
+    public int getCount() {
+        return allCats.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return allCats.get(position).getStrCategory();
+    }
 }
