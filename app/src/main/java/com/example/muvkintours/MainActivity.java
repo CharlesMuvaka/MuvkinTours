0bca196fca5e19031520a76118da0a04ba5093cd
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-11 14:13:18 +0300

    create a firebase database instance to access the database

--- app/src/main/java/com/example/muvkintours/MainActivity.java
+++ app/src/main/java/com/example/muvkintours/MainActivity.java
@@ -3,15 +3,26 @@ package com.example.muvkintours;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.muvkintours.databinding.ActivityMainBinding;
+import com.example.muvkintours.mealApi.Constants;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     private ActivityMainBinding mainBind;
+    private FirebaseDatabase muvkin;
+    private DatabaseReference ref;
+    private SharedPreferences store;
+    private SharedPreferences.Editor storeEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +30,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mainBind = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mainBind.getRoot());
 
+        muvkin = FirebaseDatabase.getInstance();
+        ref = muvkin.getReference();
+
         mainBind.btnLogin.setOnClickListener(this);
         mainBind.btnSign.setOnClickListener(this);
 
-
+        store = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        storeEditor = store.edit();
 
 
     }
@@ -40,7 +55,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
 
             Intent mealIntent = new Intent(MainActivity.this, MealActivity.class );
-            mealIntent.putExtra("userName", userName.getEditText().getText().toString());
+//            mealIntent.putExtra("userName", userName.getEditText().getText().toString());
+            storeEditor.putString(Constants.USERNAME,userName.getEditText().getText().toString()).apply();
+            Log.d("TAG", Constants.USERNAME.toString() );
             startActivity(mealIntent);
 
         }else{
