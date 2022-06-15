9d52c48a2ed8092acdf21ff81af4dcfe154ba0fe
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-15 15:32:08 +0300

    initialize a firebase authlistener to create new users

--- app/src/main/java/com/example/muvkintours/SignUpActivity.java
+++ app/src/main/java/com/example/muvkintours/SignUpActivity.java
@@ -1,24 +1,30 @@
 package com.example.muvkintours;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Toast;
 
-import com.example.muvkintours.databinding.ActivitySignUpBinding;
-import com.example.muvkintours.models.User;
-import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-import org.parceler.Parcels;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.muvkintours.databinding.ActivitySignUpBinding;
+import com.example.muvkintours.mealApi.Constants;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
     ActivitySignUpBinding signBind;
-    private DatabaseReference ref;
+    FirebaseAuth myAuth;
+    FirebaseAuth.AuthStateListener myAuthListener;
+    SharedPreferences myData;
+    SharedPreferences.Editor myDataEditor;
+
 
 
     @Override
@@ -27,47 +33,77 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         signBind = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(signBind.getRoot());
 
-        ref = FirebaseDatabase.getInstance().getReference();
+        myData = PreferenceManager.getDefaultSharedPreferences(this);
+        myDataEditor = myData.edit();
 
         signBind.btnSubmit.setOnClickListener(this);
 
+        FirebaseApp.initializeApp(this);
+        myAuth = FirebaseAuth.getInstance();
+        createAuthListener();
 
     }
 
     @Override
     public void onClick(View v) {
         if (v == signBind.btnSubmit){
-
-            Validator validate = new Validator();
-            TextInputLayout userName = signBind.userName;
-            TextInputLayout userPhone = signBind.userPhone;
-            TextInputLayout userSeat = signBind.userSeat;
-            TextInputLayout userTicket = signBind.userTicket;
-            TextInputLayout userCoach = signBind.userCoach;
-
-
-           if (!validate.validateName(userName) || !validate.validatePhone(userPhone) || !validate.validateName(userSeat) || !validate.validateName(userTicket) || !validate.validateName(userCoach)){
-               return;
-           }else{
-
-               String name = userName.getEditText().getText().toString();
-               String phone = userPhone.getEditText().getText().toString();
-               String seat = userSeat.getEditText().getText().toString();
-               String ticket = userTicket.getEditText().getText().toString();
-               String coach = userCoach.getEditText().getText().toString();
-
-               User newUser = new User(name, phone, ticket, seat, coach);
-
-               ref.child(phone).setValue(newUser);
-
-               Intent meals = new Intent(SignUpActivity.this, MealActivity.class);
-               meals.putExtra("userDetails", Parcels.wrap(newUser));
-               Toast.makeText(SignUpActivity.this, "Details received Successfully", Toast.LENGTH_SHORT).show();
-               startActivity(meals);
-           }
+            createUser();
         }
-
     }
 
+    public void createUser(){
 
+        String email = Objects.requireNonNull(signBind.userTicket.getEditText()).getText().toString().trim();
+        String password = Objects.requireNonNull(signBind.userSeat.getEditText()).getText().toString().trim();
+        String name = Objects.requireNonNull(signBind.userName.getEditText()).getText().toString().trim();
+        String phone = Objects.requireNonNull(signBind.userPhone.getEditText()).getText().toString().trim();
+
+        Validator validate = new Validator();
+
+        if (!validate.validateEmail(signBind.userTicket) || !validate.validatePassword(signBind.userSeat, signBind.userCoach) || !validate.validateName(signBind.userName) || !validate.validatePhone(signBind.userPhone)){
+            return;
+        }
+
+        myDataEditor.putString(Constants.USERNAME, name).apply();
+        myDataEditor.putString(Constants.USERPHONE, phone).apply();
+        myDataEditor.putString(Constants.USERPASSWORD, password).apply();
+        myDataEditor.putString(Constants.USEREMAIL, email).apply();
+
+
+
+        myAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()){
+                Toast.makeText(this, "User Created successfully", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "User not created", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    public void createAuthListener(){
+        myAuthListener = firebaseAuth -> {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+
+            if (user != null){
+
+                Intent newIntent = new Intent(SignUpActivity.this, MealActivity.class);
+                newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(newIntent);
+                finish();
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        myAuth.addAuthStateListener(myAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        myAuth.removeAuthStateListener(myAuthListener);
+    }
 }
