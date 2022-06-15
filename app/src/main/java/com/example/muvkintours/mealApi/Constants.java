b69c92f6aa2eeb695643fe4d24168cbd1a29a20b
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-15 15:34:31 +0300

    create constant variables to store data in firebase and SharedPreferences

--- app/src/main/java/com/example/muvkintours/mealApi/Constants.java
+++ app/src/main/java/com/example/muvkintours/mealApi/Constants.java
@@ -4,4 +4,10 @@ public class Constants {
 
     public static final String URL = "https://www.themealdb.com/api/json/v1/1/";
     public static final String USERNAME = "userName";
+    public static final String USERS = "users";
+    public static final String CATEGORIES = "categories";
+
+    public static final String USEREMAIL= "email";
+    public static final String USERPASSWORD= "password";
+    public static final String USERPHONE= "phone";
 }
