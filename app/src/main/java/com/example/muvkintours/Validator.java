6e16ffa4f88e548d9a629a1fc8583536f4f9b80e
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-15 15:32:53 +0300

    add validation to the email and password edit texts

--- app/src/main/java/com/example/muvkintours/Validator.java
+++ app/src/main/java/com/example/muvkintours/Validator.java
@@ -1,5 +1,7 @@
 package com.example.muvkintours;
 
+import android.util.Patterns;
+
 import com.google.android.material.textfield.TextInputLayout;
 
 public class Validator {
@@ -34,5 +36,70 @@ public class Validator {
 
         }
 
+
+    }
+
+    public boolean validateEmail(TextInputLayout userEmail){
+
+        String email = userEmail.getEditText().getText().toString().trim();
+
+        if (email.isEmpty()){
+            userEmail.setError("Please Enter your username");
+            return false;
+        }else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            userEmail.setError("Please enter a valid email addrress");
+            return  false;
+        }
+        else{
+            userEmail.setError(null);
+            userEmail.setErrorEnabled(false);
+            return true;
+
+        }
+
+    }
+
+    public boolean validatePassword(TextInputLayout pass1, TextInputLayout pass2){
+
+        String pass = pass1.getEditText().getText().toString().trim();
+        String password = pass2.getEditText().getText().toString().trim();
+
+        if (pass.isEmpty() ){
+            pass1.setError("Please Enter your password");
+            return false;
+        }else if (password.isEmpty() ){
+            pass2.setError("Please confirm your password");
+            return false;
+        }else if (pass.length() <= 5 ){
+            pass1.setError("Password should be more than 5 characters");
+            return false;
+        }else if (!pass.equals(password)){
+            pass2.setError("Passwords should match");
+            return  false;
+        }
+        else{
+            pass1.setError(null);
+            pass2.setError(null);
+            pass1.setErrorEnabled(false);
+            pass2.setErrorEnabled(false);
+            return true;
+
+        }
+
+    }
+
+    public boolean validatePassOnly(TextInputLayout pass1) {
+
+        String pass = pass1.getEditText().getText().toString().trim();
+
+
+        if (pass.isEmpty()) {
+            pass1.setError("Please Enter your password");
+            return false;
+        }else{
+            pass1.setError(null);
+            pass1.setErrorEnabled(false);
+            return  true;
+        }
     }
 }
