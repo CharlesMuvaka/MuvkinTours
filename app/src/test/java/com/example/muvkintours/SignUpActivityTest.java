2b58b067b34b0c82256bc2a4d06904a312e15efa
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-15 15:43:40 +0300

    create tests that add inputs to the user sign in edit fields

--- app/src/test/java/com/example/muvkintours/SignUpActivityTest.java
+++ app/src/test/java/com/example/muvkintours/SignUpActivityTest.java
@@ -3,6 +3,7 @@ package com.example.muvkintours;
 import static org.junit.Assert.*;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +14,7 @@ import org.robolectric.RobolectricTestRunner;
 @RunWith(RobolectricTestRunner.class)
 public class SignUpActivityTest {
     SignUpActivity activity;
+    FirebaseAuth myAuth;
 
     @Before
     public void setUp() throws Exception {
@@ -25,7 +27,17 @@ public class SignUpActivityTest {
     public void getHint_Returns_true(){
 
         TextInputLayout user = activity.findViewById(R.id.userName);
+        TextInputLayout userPhone = activity.findViewById(R.id.userPhone);
+        TextInputLayout userEmail = activity.findViewById(R.id.userTicket);
+        TextInputLayout userPassword = activity.findViewById(R.id.userSeat);
+        TextInputLayout password = activity.findViewById(R.id.userCoach);
+
 
         assertEquals("Enter you full name", user.getEditText().getHint());
+       assertEquals("Enter you mobile Number", userPhone.getEditText().getHint());
+       assertEquals("Enter your Email Address", userEmail.getEditText().getHint());
+       assertEquals("Enter your password", userPassword.getEditText().getHint());
+       assertEquals("Confirm your password", password.getEditText().getHint());
     }
+
 }
