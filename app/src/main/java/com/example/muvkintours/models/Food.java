f54e20885c4a381e787b4cb00c0558d5648ee79f
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-15 15:12:55 +0300

    add a searchView widget in mane items

---
+++ app/src/main/java/com/example/muvkintours/models/Food.java
@@ -0,0 +1,40 @@
+
+package com.example.retrofit.models;
+
+import java.util.List;
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Food {
+
+    @SerializedName("meals")
+    @Expose
+    private List<Meal> meals = null;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Food() {
+    }
+
+    /**
+     * 
+     * @param meals
+     */
+    public Food(List<Meal> meals) {
+        super();
+        this.meals = meals;
+    }
+
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
+    }
+
+}
